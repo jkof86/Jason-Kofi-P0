d57bc271d3ae9e9235fc5010a43d091de5ed83b8
jkof86 <jason.kofi@revature.net>
2023-01-26 18:04:38 -0500

    Tested File and FileWriter

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,5 +1,13 @@
 package com.revature;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.revature.model.Employee;
+
 /**
  * Hello world!
  */
@@ -13,5 +21,38 @@ public final class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Employee emp = new Employee();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            System.out.println(mapper.writeValueAsString(emp));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        File f = new File("test.txt");
+        if (f.exists()){System.out.println("File already exists");}
+
+        else{
+
+        try {
+            f.createNewFile();
+            f.exists();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write("Writing to the second file");
+            fw.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
+    }
+
 }
