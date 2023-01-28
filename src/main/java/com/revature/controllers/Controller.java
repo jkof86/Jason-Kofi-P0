55a9bfab9237af5c9b9bdc904e328b3ce79dcbf5
jkof86 <jason.kofi@revature.net>
2023-01-27 23:40:19 -0500

    Set up methods for GET POST and PUT requests

--- src/main/java/com/revature/controllers/Controller.java
+++ src/main/java/com/revature/controllers/Controller.java
@@ -1,44 +1,108 @@
 package com.revature.controllers;
+
 import java.io.IOException;
 import java.io.OutputStream;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-
 //This class holds the methods to handle http requests
-public class Controller implements HttpHandler{
+public class Controller implements HttpHandler {
 
-    //Handle method will execute once I receive the right request
+    // Handle method will execute once I receive the right request
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-       
-        String someResponse = "This is a response using Strings!";
+
+        // String someResponse = "This is a response using Strings!";
         String someResponse2 = "Sorry, we're only accepting \"GET\" requests at the moment.";
 
         String httpVerb = exchange.getRequestMethod();
-        
 
-        switch (httpVerb){
+        switch (httpVerb) {
             case "GET":
-            exchange.getRequestBody();
-            exchange.sendResponseHeaders(200, someResponse.getBytes().length);
+                getRequest(exchange);
+                break;
 
-            //we have to save the string into a class that httpServer can handle
-            OutputStream os = exchange.getResponseBody();
-            os.write(someResponse.getBytes()); //writing inside the response body
-            os.close();
-            break;
+                case "POST":
+                postRequest(exchange);
+                break;
 
-            default:
-            exchange.getRequestBody();
-            exchange.sendResponseHeaders(400, someResponse2.getBytes().length);
-
-            OutputStream os2 = exchange.getResponseBody();
-            os2.write(someResponse2.getBytes()); //writing inside the response body
-            os2.close();
-            break;
+                case "PUT":
+                putRequest(exchange);
+                break;
+                
+                default:
+                otherRequest(exchange);
+                break;
         }
     }
-    
+
+    public void getRequest(HttpExchange exchange) throws IOException {
+
+        String getResponse = "You selected the \"GET\" response";
+
+        // first we retrieve the request body from the exchange
+        exchange.getRequestBody();
+
+        // then we create a response
+        exchange.sendResponseHeaders(200, getResponse.getBytes().length);
+        // we have to save the string into a class that httpServer can handle
+        OutputStream os = exchange.getResponseBody();
+
+        os.write(getResponse.getBytes()); // writing inside the response body
+        os.close(); // make sure to close the output stream
+
+    }
+
+    public void postRequest(HttpExchange exchange) throws IOException {
+
+        String getResponse = "You selected the \"POST\" response";
+
+        // first we retrieve the request body from the exchange
+        exchange.getRequestBody();
+
+        // then we create a response
+        exchange.sendResponseHeaders(200, getResponse.getBytes().length);
+        // we have to save the string into a class that httpServer can handle
+        OutputStream os = exchange.getResponseBody();
+
+        os.write(getResponse.getBytes()); // writing inside the response body
+        os.close(); // make sure to close the output stream
+
+    }
+
+    public void putRequest(HttpExchange exchange) throws IOException {
+
+        String getResponse = "You selected the \"PUT\" response";
+
+        // first we retrieve the request body from the exchange
+        exchange.getRequestBody();
+
+        // then we create a response
+        exchange.sendResponseHeaders(200, getResponse.getBytes().length);
+        // we have to save the string into a class that httpServer can handle
+        OutputStream os = exchange.getResponseBody();
+
+        os.write(getResponse.getBytes()); // writing inside the response body
+        os.close(); // make sure to close the output stream
+
+    }
+
+    public void otherRequest(HttpExchange exchange) throws IOException {
+
+        String getResponse = "Sorry, we're only accepting \"GET\", \"POST\", and \"PUT\" requests at the moment.";
+
+        // first we retrieve the request body from the exchange
+        exchange.getRequestBody();
+
+        // then we create a response
+        exchange.sendResponseHeaders(400, getResponse.getBytes().length);
+        // we have to save the string into a class that httpServer can handle
+        OutputStream os = exchange.getResponseBody();
+
+        os.write(getResponse.getBytes()); // writing inside the response body
+        os.close(); // make sure to close the output stream
+
+    }
+
 }
