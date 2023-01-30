6c789fa2200ca0f66f37fedd82bab93f51300086
jkof86 <jason.kofi@revature.net>
2023-01-30 18:36:37 -0500

    Added BufferedWriter and PrintWriter to append to existing log file

--- src/main/java/com/revature/repository/EmployeeRepository.java
+++ src/main/java/com/revature/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.revature.repository;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -24,13 +26,24 @@ public class EmployeeRepository{
     }
 
     //here we will save each employee object that's created, locally
-    public void log(String s){
+    public void log(String s) throws IOException{
 
-        try {
-            FileWriter fw = new FileWriter("./log.txt");
-            fw.append(s.toString());
-            fw.close();
+        //the true parameter allows us to append to the end of the log file
+        //we use BufferedWriter and PrintWriter to write to an existing file
+        try ( FileWriter fw = new FileWriter("./log.txt", true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter pw = new PrintWriter(bw); ) 
+        
+        { 
+            //we add a line break between entries
+            pw.println();
+            pw.println(s.toString()); 
+        }
 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    
             //if this block of code executes, then we update the 
             //log and return an http response to the end user
             exchange.getRequestBody();
@@ -38,27 +51,20 @@ public class EmployeeRepository{
             //we create the response in a new variable so we can verify the correct response length 
             //in the second response header parameter
             String response1 = "The local log has been updated with the the user: " + s.toString();
-            exchange.sendResponseHeaders(200, response1.getBytes().length);
             
+            exchange.sendResponseHeaders(200, response1.getBytes().length);
+                       
             //we create the stream and write it to the response body
             OutputStream os = exchange.getResponseBody();
             
             //we made sure to use response1 so the .length is correct
             os.write(response1.getBytes());
             os.close();
-            
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
     }
 
     public void register(Employee e){
         //first we save the
 
     }
-
-        
     
 }
